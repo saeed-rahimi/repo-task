602ca260b23da974648e6645e238ef9d685296b5
saeed rahimi <saeedteachcodee@email.com>
2024-10-29 08:33:38 +0330

    fix

--- Short-CircuitingwithLogicalOperators‏/script.js
+++ Short-CircuitingwithLogicalOperators‏/script.js
@@ -1,1 +1,16 @@
 " use strict"
+const user1 = { username: "JaneDoe", language: "Spanish" };
+const user2 = {};
+
+
+function getWelcomeMessage(user) {
+  const username = user.username || "Guest";
+  const language = user.language || "English";
+  
+  // 2. Return the welcome message.
+  return `Hello, ${username}! Welcome to our site in ${language}.`;
+}
+
+
+console.log(getWelcomeMessage(user1)); 
+console.log(getWelcomeMessage(user2)); 
