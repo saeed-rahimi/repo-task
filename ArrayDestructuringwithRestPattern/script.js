681af069dc517698fafa78da59c2258fbc74372d
saeed rahimi <saeedteachcodee@email.com>
2024-10-29 08:19:42 +0330

    fix

--- ArrayDestructuringwithRestPattern/script.js
+++ ArrayDestructuringwithRestPattern/script.js
@@ -1,11 +1,9 @@
 "use strict"
 const scores = [98, 85, 91, 78, 87, 96, 72];
 
-// 1. Destructure the first three scores as `top1`, `top2`, and `top3`.
+
+
 const [top1, top2, top3, ...otherScores] = scores;
 
-// 2. `otherScores` will collect the rest of the scores in an array.
-
-// 3. Log the top scores and the rest of the scores separately.
 console.log("Top scores:", top1, top2, top3);
 console.log("Other scores:", otherScores);
