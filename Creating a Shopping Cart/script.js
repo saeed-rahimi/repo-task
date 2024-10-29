51df4fe319965b2897962077d1082d6b14764a21
saeed rahimi <saeedteachcodee@email.com>
2024-10-29 09:36:32 +0330

    finish

--- Creating a Shopping Cart/script.js
+++ Creating a Shopping Cart/script.js
@@ -1,1 +1,27 @@
-"use stict"
+"use strict"
+const  cartItems=[
+    {name :"Shoes" ,quantity:2 ,price :75},
+    {name:"Hat" ,price : 20},
+    {name:"Socks" ,quantity:3},
+  
+]
+
+
+cartItems.forEach(item => {
+
+    const { 
+        name, 
+        quantity = 1, 
+        price = 10 
+    } = {
+        name: item.name,
+        quantity: item.quantity ?? 1,
+        price: item.price ?? 10
+    };
+    
+ 
+    const totalCost = quantity * price;
+    
+    
+    console.log(`Item: ${name}, Quantity: ${quantity}, Price: ${price}, Total Cost: ${totalCost}`);
+});
